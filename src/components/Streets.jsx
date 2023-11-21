a8bf598bebc7c71780f998f063126c0c04055959
AnnaAnghalatyan <48157696+AnnaAnghalatyan@users.noreply.github.com>
2023-11-21 21:01:03 +0400

    Update Streets.jsx

--- src/components/Streets.jsx
+++ src/components/Streets.jsx
@@ -1,21 +1,10 @@
-import { Collapse } from 'antd';
+import {Collapse, Empty} from 'antd';
 import {useEffect, useState} from "react";
 import { getStreets } from "../utils/api";
+import Houses from "./Houses";
 
-
-const itemsNest = [
-    {
-        key: '1',
-        label: 'This is panel nest panel',
-        children: <p>test</p>,
-    },
-];
-
-
-
-function List() {
+function Streets() {
     const [streets, setStreets] = useState([]);
-    const [buildings, setBuildings] = useState([]);
 
     useEffect(() => {
         getStreets().then(res => {
@@ -23,15 +12,16 @@ function List() {
         })
     }, [])
 
-    const onChange = (key) => {
-        console.log(key);
-    };
 
-    return <Collapse onChange={onChange} items={streets.map((street, index) => ({
-        key: index + 1,
-        label: street.name,
-        children: <Collapse defaultActiveKey="1" items={itemsNest} />,
-    }))} />;
+    if (streets.length) {
+        return <Collapse items={streets.map((street, index) => ({
+            key: index + 1,
+            label: street.name,
+            children: <Houses streetId={street.id} />,
+        }))} />
+    }
+
+    return <Empty />
 }
 
-export default List
+export default Streets
